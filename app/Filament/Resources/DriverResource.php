cf49e8c5c85cb5aa4271752eca8639fc348c6a45
ujiherliana <ujiherliana@gmail.com>
2023-12-06 22:52:23 +0700

    initialisation laravel-filament

---
+++ app/Filament/Resources/DriverResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DriverResource\Pages;
+use App\Filament\Resources\DriverResource\RelationManagers;
+use App\Models\Driver;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class DriverResource extends Resource
+{
+    protected static ?string $model = Driver::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Select::make('gender')
+                    ->required()
+                    ->options([
+                        'Pria' => 'Pria',
+                        'Wanita' => 'Wanita'
+                    ]),
+                Forms\Components\Select::make('job')
+                    ->required()
+                    ->options([
+                        'Motor' => 'Motor',
+                        'Mobil' => 'Mobil'
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('gender')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('job')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDrivers::route('/'),
+            'create' => Pages\CreateDriver::route('/create'),
+            'edit' => Pages\EditDriver::route('/{record}/edit'),
+        ];
+    }
+}
